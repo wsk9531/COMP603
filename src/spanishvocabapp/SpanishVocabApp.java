23f7ffe3f7bfd0a3c22b807c7aadf9e8997c648e
Ben <Ben@DESKTOP-GAGPDRQ>
2020-06-28 19:30:10 +1200

    Added textual game function that works with DB calls

--- src/spanishvocabapp/SpanishVocabApp.java
+++ src/spanishvocabapp/SpanishVocabApp.java
@@ -5,29 +5,57 @@
  */
 package spanishvocabapp;
 
+import java.util.Scanner;
+
 /**
  * On Github at https://github.com/wsk9531/COMP603
+ *
  * @author Ben Henshall
  * @id 14867281
  */
 public class SpanishVocabApp {
-    
+
+    boolean continueFlag;
+
     public static void main(String[] args) {
         DBTools db = new DBTools();
-        
+        System.out.println("Database connection working: " + db);
+
         boolean tableNeedsFillingFlag = db.checkAndCreateTable();
         if (tableNeedsFillingFlag) {
             db.populateTableFromText();
         }
-        
+
         WordList gameList = new WordList(db);
         System.out.println(gameList.getWordList());
-        
-       //gameList.IncrementLeitnerLevel(gameList.getWordList().get(0), db);
-      // gameList.IncrementLeitnerLevel(gameList.getWordList().get(0), db);
-       //gameList.ResetLeitnerLevel(gameList.getWordList().get(0), db);
-        
-        
-        System.out.println("Database connection working: " + db);
+
+        runGame(gameList, db);
+
+    }
+
+    public static void runGame(WordList gameList, DBTools db) {
+        System.out.println("What do the following 10 Spanish words mean in English?");
+        System.out.println("Type your answer, then press enter to move on. Good Luck!");
+        Scanner scan = new Scanner(System.in);
+
+        //work through the 10 values present
+        for (int i = 0; i < 10; i++) {
+            int shuffledIndexNum = gameList.getOrderMixer().get(i);
+            Word tested = gameList.getWordList().get(shuffledIndexNum);
+
+            System.out.println(tested.getSpanishMeaning() + "? Answer:");
+            String scannerGameInput = scan.nextLine();
+
+            if (scannerGameInput.trim().equalsIgnoreCase(tested.getEnglishMeaning())) {
+                System.out.println("Correct!");
+                gameList.IncrementLeitnerLevel(tested, db);
+
+            } else {
+                System.out.println("Incorrect!");
+                gameList.ResetLeitnerLevel(tested, db);
+            }
+        }
     }
 }
+
+
